e00a1dde9cc7faf22c870afbd74605e1fbfab2a8
meow-d <isaacwenyi@gmail.com>
2024-10-23 14:26:27 +0800

    Move backend test code to Backend.java

--- src/App.java
+++ src/App.java
@@ -1,32 +1,7 @@
-import data.*;
-import database.Database;
+import user_interface.*;
 
 public class App {
     public static void main(String[] args) {
-        Database db = new Database();
-
-        Account account = new Account("admin", "admin", Role.ADMIN);
-        db.addAccount(account);
-
-        Item item = new Item("CODE123", "Item 1", 1, 100, 20);
-        db.addItem(item);
-
-        Supplier supplier = new Supplier("SUP123", "Supplier 1", "contact@supplier1.com");
-        db.addSupplier(supplier);
-
-        Sale sale = new Sale(item.getId(), 10, new java.util.Date(), 1);
-        db.addSale(sale);
-
-        PurchaseRequisition pr = new PurchaseRequisition(item.getId(), 50, new java.util.Date(), 1, Status.PENDING);
-        db.addPurchaseRequisition(pr);
-
-        PurchaseOrder po = new PurchaseOrder(
-                pr.getId(),
-                supplier.getId(),
-                account.getId(),
-                "pending",
-                new java.util.Date(),
-                500.00);
-        db.addPurchaseOrder(po);
+        UserInterface ui = new UserInterface();
     }
 }
