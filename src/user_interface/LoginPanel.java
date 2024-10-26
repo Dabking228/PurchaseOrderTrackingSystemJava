a1eb7e17ed04a93034e5d2fe6d4c02cb7e26eaf8
meow-d <isaacwenyi@gmail.com>
2024-10-26 11:41:58 +0800

    Basic UI setup

---
+++ src/user_interface/LoginPanel.java
@@ -0,0 +1,10 @@
+package user_interface;
+
+import javax.swing.*;
+import java.awt.*;
+import backend.*;
+
+public class LoginPanel extends JPanel {
+    LoginPanel() {
+    }
+}
--- src/user_interface/UserInterface.java
+++ src/user_interface/UserInterface.java
@@ -7,7 +7,11 @@ import backend.Backend;
 
 public class UserInterface {
     private JFrame frame;
+    private Backend backend;
     private CardLayout cardLayout;
+
+    private JPanel mainPanel;
+    private LoginPanel loginPanel;
 
     public UserInterface() {
         initializeSystem();
@@ -15,10 +19,56 @@ public class UserInterface {
     }
 
     private void initializeSystem() {
-        Backend backend = new Backend();
+        backend = new Backend();
     }
 
     private void initializeUserInterface() {
-        // TODO
+        // Set theme
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException
+                | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        // Initialize frame
+        cardLayout = new CardLayout();
+        frame = new JFrame("AHHASC System");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                backend.save();
+                System.exit(0);
+            }
+        });
+        frame.setSize(1100, 500);
+
+        // Setup the main panel
+        mainPanel = new JPanel();
+        mainPanel.setLayout(cardLayout);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        loginPanel = new LoginPanel();
+        mainPanel.add(loginPanel, "login");
+
+        frame.setContentPane(mainPanel);
+
+        showPanel("login");
+    }
+
+    public void showPanel(String panelName) {
+        // TODO Custom behaviour if we need it
+        // if (panelName.equals("login")) {
+        // }
+
+        cardLayout.show(mainPanel, panelName);
+        // frame.pack();
+        frame.setVisible(true);
     }
 }
