dcc88f3b3c01e4216947b0a61428957c09ca9d5a
meow-d <isaacwenyi@gmail.com>
2024-11-12 17:40:12 +0800

    Fix CSV bug where accounts are saved as `password, role, username` but read as `username, password, role`

--- src/data/Account.java
+++ src/data/Account.java
@@ -4,6 +4,15 @@ public class Account extends BaseItem {
     private String password;
     private Role role;
 
+    // Constructor
+    // Don't change the order of these two or else CSV will break
+    // Yea it's kinda jank
+    public Account(String password, Role role, String username) {
+        this.password = password;
+        this.role = role;
+        this.Id = username;
+    }
+
     public Account(String username, String password, Role role) {
         this.password = password;
         this.role = role;
