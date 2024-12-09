f0b892281e4785e768c03b2c6d342de90599b743
potatoonions <darkenbrine@gmail.com>
2024-12-09 22:27:02 +0800

    Added add new user

---
+++ src/user_interface/panels/AddNewUser.java
@@ -0,0 +1,44 @@
+package user_interface.panels;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import backend.Backend;
+import user_interface.MainMenu;
+
+public class AddNewUser extends JPanel {
+    public AddNewUser(Backend backend, MainMenu parent) {
+        
+        setLayout(null);
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setBounds(10, 10, 100, 25);
+        add(usernameLabel);
+
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(120, 10, 150, 25);
+        add(usernameField);
+
+        JLabel roleLabel = new JLabel("Role:");
+        roleLabel.setBounds(10, 50, 100, 25);
+        add(roleLabel);
+
+        String[] roles = {"Admin", "Finance Manager", "Inventory Manager", "Purchase Manager", "Sales Manager"};
+        JComboBox<String> roleComboBox = new JComboBox<>(roles);
+        roleComboBox.setBounds(120, 50, 150, 25);
+        add(roleComboBox);
+
+        JButton confirmButton = new JButton("Add User");
+        confirmButton.setBounds(10, 90, 120, 25);
+        confirmButton.addActionListener(e -> {
+            // Logic to add the user
+            String username = usernameField.getText();
+            String role = (String) roleComboBox.getSelectedItem();
+            // Ad thisser to the backend
+            System.out.println("Adding user: " + username + " with role: " + role);
+        });
+        add(confirmButton);
+    }
+}
