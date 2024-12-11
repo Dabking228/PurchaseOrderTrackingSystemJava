75fafa952d57a781973db37c1d41a09785177e7c
Mcproking <shanefoomg@gmail.com>
2024-12-12 01:23:28 +0800

    edited show panel to be public

--- src/user_interface/MainMenu.java
+++ src/user_interface/MainMenu.java
@@ -78,7 +78,7 @@ public class MainMenu extends JPanel {
         }
     }
 
-    void showPanel(String panelName) {
+    public void showPanel(String panelName) {
         cardLayout.show(this, panelName);
 
         Component currentPanel = this.getCurrentComponent(this);
