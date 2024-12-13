2d0c68a47103ab8246871b827d41e4ced7622d89
Dabking228 <darylsim228@gmail.com>
2024-12-13 16:49:43 +0800

    change "Purchace" to "Purchase"

--- src/data/PurchaseManager.java
+++ src/data/PurchaseManager.java
@@ -4,16 +4,16 @@ package data;
  *
  * @author Keith Lo Ze Hui
  */
-public class PurchaceManager extends Account {
-    public PurchaceManager(String username, String password) {
+public class PurchaseManager extends Account {
+    public PurchaseManager(String username, String password) {
         super(username, password, Role.PURCHASE_MANAGER);
     }
 
-    public PurchaceManager(String password, Role role, String Id) {
+    public PurchaseManager(String password, Role role, String Id) {
         super(password, role, Id);
     }
 
-    public PurchaceManager(String username, String password, Role role) {
+    public PurchaseManager(String username, String password, Role role) {
         super(username, password, role);
     }
 
