ddfd6d2e4866fd8fb90cf09a04e938e0f3ed55d9
potatoonions <darkenbrine@gmail.com>
2024-12-14 18:13:06 +0800

    added

--- src/user_interface/panels/StockEntry.java
+++ src/user_interface/panels/StockEntry.java
@@ -4,18 +4,26 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
+import java.awt.Color;
 
 import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
+import javax.swing.Timer;
+import javax.swing.JLabel;
 
 import backend.Backend;
 import data.*;
 import user_interface.MainMenu;
 public class StockEntry extends data<Item> {
 
+    public StockEntry() {
+    }
+
+    public Item promptForItemDetails() {
+    }
 
     public Item promptForItemDetails() {
         
@@ -36,6 +44,24 @@ public class StockEntry extends data<Item> {
 
         FieldText fieldReorderLevel = new FieldText("Reorder Level", true);
         contentPanel.add(fieldReorderLevel);
+
+        JButton confirmButton = new JButton("Confirm");
+        confirmButton.addActionListener(e -> {
+            String itemCode = fieldItemCode.getText();
+            String itemName = fieldItemName.getText();
+            String supplierId = fieldSupplierId.getText();
+            int stockLevel = Integer.parseInt(fieldStockLevel.getText());
+            int reorderLevel = Integer.parseInt(fieldReorderLevel.getText());
+
+            backend.db.addItem(new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel));
+            JLabel successLabel = new JLabel("Item added successfully!");
+            successLabel.setForeground(Color.GREEN);
+            contentPanel.add(successLabel);
+            Timer timer = new Timer(2000, g -> successLabel.setVisible(false));
+            timer.setRepeats(false);
+            timer.start();
+        });
+        contentPanel.add(confirmButton);
 
         int result = JOptionPane.showConfirmDialog(null, contentPanel, "Enter Item Details", JOptionPane.OK_CANCEL_OPTION);
         if (result == JOptionPane.OK_OPTION) {
