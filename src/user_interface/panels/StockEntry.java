00c14f6bb0920436e730256eb3cad838f9cbf548
Mcproking <shanefoomg@gmail.com>
2024-12-15 01:05:35 +0800

    use correct panel

--- src/user_interface/panels/StockEntry.java
+++ src/user_interface/panels/StockEntry.java
@@ -19,59 +19,59 @@ import data.*;
 import user_interface.MainMenu;
 public class StockEntry extends Panel<Item> {
 
-    public StockEntry() {
+    public StockEntry(Backend backend, MainMenu parent) {
+        super("");
     }
 
-    public Item promptForItemDetails() {
-    }
+    // public Item promptForItemDetails() {
+    // }
 
-    public Item promptForItemDetails() {
-        super();
+    // public Item promptForItemDetails() {
 
-        FieldText fieldItemCode = new FieldText("Item Code");
-        contentPanel.add(fieldItemCode);
+    //     FieldText fieldItemCode = new FieldText("Item Code");
+    //     contentPanel.add(fieldItemCode);
 
-        FieldText fieldItemName = new FieldText("Item Name");
-        contentPanel.add(fieldItemName);
+    //     FieldText fieldItemName = new FieldText("Item Name");
+    //     contentPanel.add(fieldItemName);
 
-        FieldText fieldSupplierId = new FieldText("Supplier ID");
-        contentPanel.add(fieldSupplierId);
+    //     FieldText fieldSupplierId = new FieldText("Supplier ID");
+    //     contentPanel.add(fieldSupplierId);
 
-        FieldText fieldStockLevel = new FieldText("Stock Level", true);
-        contentPanel.add(fieldStockLevel);
+    //     FieldText fieldStockLevel = new FieldText("Stock Level", true);
+    //     contentPanel.add(fieldStockLevel);
 
-        FieldText fieldReorderLevel = new FieldText("Reorder Level", true);
-        contentPanel.add(fieldReorderLevel);
+    //     FieldText fieldReorderLevel = new FieldText("Reorder Level", true);
+    //     contentPanel.add(fieldReorderLevel);
 
-        JButton confirmButton = new JButton("Confirm");
-        confirmButton.addActionListener(e -> {
-            String itemCode = fieldItemCode.getText();
-            String itemName = fieldItemName.getText();
-            String supplierId = fieldSupplierId.getText();
-            int stockLevel = Integer.parseInt(fieldStockLevel.getText());
-            int reorderLevel = Integer.parseInt(fieldReorderLevel.getText());
+    //     JButton confirmButton = new JButton("Confirm");
+    //     confirmButton.addActionListener(e -> {
+    //         String itemCode = fieldItemCode.getText();
+    //         String itemName = fieldItemName.getText();
+    //         String supplierId = fieldSupplierId.getText();
+    //         int stockLevel = Integer.parseInt(fieldStockLevel.getText());
+    //         int reorderLevel = Integer.parseInt(fieldReorderLevel.getText());
 
-            backend.db.addItem(new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel));
-            JLabel successLabel = new JLabel("Item added successfully!");
-            successLabel.setForeground(Color.GREEN);
-            contentPanel.add(successLabel);
-            Timer timer = new Timer(2000, g -> successLabel.setVisible(false));
-            timer.setRepeats(false);
-            timer.start();
-        });
-        contentPanel.add(confirmButton);
+    //         backend.db.addItem(new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel));
+    //         JLabel successLabel = new JLabel("Item added successfully!");
+    //         successLabel.setForeground(Color.GREEN);
+    //         contentPanel.add(successLabel);
+    //         Timer timer = new Timer(2000, g -> successLabel.setVisible(false));
+    //         timer.setRepeats(false);
+    //         timer.start();
+    //     });
+    //     contentPanel.add(confirmButton);
 
-        int result = JOptionPane.showConfirmDialog(null, contentPanel, "Enter Item Details", JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-            String itemCode = fieldItemCode.getText();
-            String itemName = fieldItemName.getText();
-            String supplierId = fieldSupplierId.getText();
-            int stockLevel = Integer.parseInt(fieldStockLevel.getText());
-            int reorderLevel = Integer.parseInt(fieldReorderLevel.getText());
+    //     int result = JOptionPane.showConfirmDialog(null, contentPanel, "Enter Item Details", JOptionPane.OK_CANCEL_OPTION);
+    //     if (result == JOptionPane.OK_OPTION) {
+    //         String itemCode = fieldItemCode.getText();
+    //         String itemName = fieldItemName.getText();
+    //         String supplierId = fieldSupplierId.getText();
+    //         int stockLevel = Integer.parseInt(fieldStockLevel.getText());
+    //         int reorderLevel = Integer.parseInt(fieldReorderLevel.getText());
 
-            return new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel);
-        }
-        return null; 
-    }
+    //         return new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel);
+    //     }
+    //     return null; 
+    // }
 
 }
