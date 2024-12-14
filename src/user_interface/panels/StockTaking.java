78b16a9641c3aa289630a86e25f97892f75ff163
potatoonions <darkenbrine@gmail.com>
2024-12-14 17:23:40 +0800

    changes

--- src/user_interface/panels/StockTaking.java
+++ src/user_interface/panels/StockTaking.java
@@ -1,30 +1,21 @@
 package user_interface.panels;
 
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-
 import backend.Backend;
 import data.*;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import user_interface.MainMenu;
 public class StockTaking extends Panel<Item>{
     public StockTaking(Backend backend, MainMenu parent){
         super("Daily Stock Taking", parent, backend.db.itemsMap, backend);
 
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
+
         FieldText fieldItemID = new FieldText("Item ID");
         contentPanel.add(fieldItemID);
-
-        // JPanel contents = new JPanel();
-        // contents.setLayout(new BoxLayout(contents,BoxLayout.PAGE_AXIS));
-
         FieldText fieldSupplierName = new FieldText("Supplier Name");
         contentPanel.add(fieldSupplierName);
 
@@ -37,7 +28,7 @@ public class StockTaking extends Panel<Item>{
         FieldText fieldMinStock = new FieldText("Minimum Stock", true);
         contentPanel.add(fieldMinStock);
 
-        // contentPanel.add(contents);
+        this.add(contentPanel, BorderLayout.CENTER);
 
         JButton confirmButton = new JButton("Confirm");
         confirmButton.addActionListener((e -> {
