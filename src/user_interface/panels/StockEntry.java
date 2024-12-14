f2e2bd8b9f6d90f1b8f386d1498ea93a5f20401f
potatoonions <darkenbrine@gmail.com>
2024-12-14 17:21:27 +0800

    changes

--- src/user_interface/panels/StockEntry.java
+++ src/user_interface/panels/StockEntry.java
@@ -18,17 +18,36 @@ public class StockEntry extends data<Item> {
 
 
     public Item promptForItemDetails() {
+        
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
 
-        String itemCode = JOptionPane.showInputDialog("Enter Item Code:");
-        String itemName = JOptionPane.showInputDialog("Enter Item Name:");
-        String supplierId = JOptionPane.showInputDialog("Enter Supplier ID:");
-        String stockLevelStr = JOptionPane.showInputDialog("Enter Stock Level:");
-        String reorderLevelStr = JOptionPane.showInputDialog("Enter Reorder Level:");
+        FieldText fieldItemCode = new FieldText("Item Code");
+        contentPanel.add(fieldItemCode);
 
-        int stockLevel = Integer.parseInt(stockLevelStr);
-        int reorderLevel = Integer.parseInt(reorderLevelStr);
+        FieldText fieldItemName = new FieldText("Item Name");
+        contentPanel.add(fieldItemName);
 
-        return new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel);
+        FieldText fieldSupplierId = new FieldText("Supplier ID");
+        contentPanel.add(fieldSupplierId);
+
+        FieldText fieldStockLevel = new FieldText("Stock Level", true);
+        contentPanel.add(fieldStockLevel);
+
+        FieldText fieldReorderLevel = new FieldText("Reorder Level", true);
+        contentPanel.add(fieldReorderLevel);
+
+        int result = JOptionPane.showConfirmDialog(null, contentPanel, "Enter Item Details", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            String itemCode = fieldItemCode.getText();
+            String itemName = fieldItemName.getText();
+            String supplierId = fieldSupplierId.getText();
+            int stockLevel = Integer.parseInt(fieldStockLevel.getText());
+            int reorderLevel = Integer.parseInt(fieldReorderLevel.getText());
+
+            return new Item(itemCode, itemName, supplierId, stockLevel, reorderLevel);
+        }
+        return null; 
     }
 
 }
