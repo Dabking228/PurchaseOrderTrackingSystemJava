c5aeecd96074ecee0f296d27cdcaf1d1dac7c74e
potatoonions <darkenbrine@gmail.com>
2024-12-15 13:27:02 +0800

    PurchaseReqPan Changes

--- src/user_interface/panels/PurchaseReqPanel.java
+++ src/user_interface/panels/PurchaseReqPanel.java
@@ -4,11 +4,33 @@ import backend.Backend;
 import user_interface.*;
 
 public class PurchaseReqPanel extends Panel<data.PurchaseRequisition> {
-
+    protected FieldText itemId, quantity, date, SaleMangID, Status, ID;
 
     public PurchaseReqPanel(Backend backend, MainMenu parent){
-       super("Purchase Requisition", parent, backend.db.purchaseRequisitionsMap, backend);
+       super("Purchase Requisition", parent, backend.db.PurchaseRequisition, backend);
        
+       FieldText itemId = new FieldText("Item ID: ");
+       contentPanel.add(itemId);
+
+       quantity = new FieldText("Item Name: ");
+       contentPanel.add(quantity);
+
+       date = new FieldText("Date");
+       contentPanel.add(date);
+
+       SaleMangID =  new FieldText("Sales Manger ID: ");
+       contentPanel.add(SaleMangID);
+
+       Status = new FieldText("Status: ");
+       contentPanel.add(Status);
+
+       ID = new FieldText("User ID: ");
+       contentPanel.add(ID);
+
+       backButton.addActionListener(e -> {
+        parent.showMainMenu();
+    });
+    
     }
     
 }
