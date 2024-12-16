b890c2569c1a1c06580d85a12116585ea39b1b41
Mcproking <shanefoomg@gmail.com>
2024-12-17 04:21:32 +0800

    refacotr account

--- src/user_interface/panels/AddUserPanel.java
+++ src/user_interface/panels/AddUserPanel.java
@@ -23,54 +23,15 @@ public class AddUserPanel extends Panel<Account> {
     protected FieldPassword fieldPassword;
     protected JLabel greenLabel, roleLabel;
     protected JComboBox<String> roleComboBox;
-    private boolean viewOnly;
     private Role role;
     protected Account account;
     private AccountList accountList;
     private String rowData;
-    private JButton editConfirm, editCancel, deleteButton, editButton;
+    private JButton editConfirm, editCancel, deleteButton, editButton, confirmButton;
     private JPanel editcfm;
 
     public AddUserPanel(Backend backend, MainMenu parent) {
-        this(backend, parent, false);
-
-        JButton confirmButton = new JButton("Add User");
-        confirmButton.addActionListener(e -> {
-            // Logic to add the user
-            try {
-                String username = fieldUsername.getData();
-                String password = fieldPassword.getPassword();
-                String roleString = (String) roleComboBox.getSelectedItem();
-                // Ad thisser to the backend
-
-                if (!username.isEmpty() || !password.isEmpty()) {
-                    
-                    backend.db.addAccount(new Account(username, password, getRoleFromString(roleString)));
-                    greenLabel.setVisible(true);
-                    System.out.println("Adding user: " + username + ":" + password + ":" + getRoleFromString(roleString)); // TODO: Remove
-
-                    // Reset field and data
-                    fieldUsername.resetField();
-                    fieldPassword.resetField();
-                    role = null;
-
-                    // timer for showing green label
-                    Timer timer = new Timer(2000, g -> greenLabel.setVisible(false));
-                    timer.setRepeats(false);
-                    timer.start();
-                }
-            } catch (Exception err) {
-                System.out.println(err);
-            }
-
-        });
-        titleButtonPanel.add(confirmButton);
-
-    }
-
-    public AddUserPanel(Backend backend, MainMenu parent, boolean viewOnly) {
         super("Add User", parent, backend.db.accountsMap, backend);
-        this.viewOnly = viewOnly;
         this.accountList = new AccountList();
         role = backend.getCurrentAccount().getRole();
 
@@ -105,100 +66,162 @@ public class AddUserPanel extends Panel<Account> {
             parent.showPanel("accountsTable");
         });
 
-        // If is a view only
-        if (viewOnly) {
+        editcfm = new JPanel(new GridLayout(1, 2));
+        editConfirm = new JButton("Confirm");
+        editCancel = new JButton("Cancel");
+        editcfm.add(editConfirm);
+        editcfm.add(editCancel);
+        editcfm.setMaximumSize(new Dimension(300, 30));
+        editcfm.setVisible(false);
+        contentPanel.add(editcfm);
+
+        editConfirm.addActionListener(e -> {
+            editcfm.setVisible(false);
+            try {
+                String username = fieldUsername.getData();
+                String password = fieldPassword.getPassword();
+                String roleString = (String) roleComboBox.getSelectedItem();
+                Role covertedRole = getRoleFromString(roleString);
+
+                if (!username.equals(account.getUsername())) {
+                    System.out.println("username changed"); // TODO: remove
+                    backend.db.accountsMap.get(account.getId()).setUsername(username);
+                }
+                if (!password.equals(account.getPassword())) {
+                    System.out.println("pass changed"); // TODO: remove
+                    account.setPassword(password);
+                }
+                if (covertedRole  != account.getRole()) {
+                    System.out.println("role changed"); // TODO: remove
+                    account.setRole(getRoleFromString(roleString));
+                }
+
+                System.out.println("item updated"); // TODO: remove
+                fieldUsername.setEditable(false);
+                fieldPassword.setEditable(false);
+                roleComboBox.setEnabled(false);
+                System.out.println(account.getUsername());
+                System.out.println(account.getPassword());
+                System.out.println(account.getRole());
+                editcfm.setVisible(false);
+            } catch (Exception err) {
+                System.out.println(err);
+            }
+
+        });
+
+        editCancel.addActionListener(e -> {
             fieldUsername.setEditable(false);
             fieldPassword.setEditable(false);
             roleComboBox.setEnabled(false);
 
-            // init item lists
-            accountList.setItem(items);
-            accountList.setValue();
-
-            if (role.hasPermission("Accounts", Permission.CREATE)) {
-                editcfm = new JPanel(new GridLayout(1, 2));
-                editConfirm = new JButton("Confirm");
-                editCancel = new JButton("Cancel");
-                editcfm.add(editConfirm);
-                editcfm.add(editCancel);
-                editcfm.setMaximumSize(new Dimension(300, 30));
-                editcfm.setVisible(false);
-                contentPanel.add(editcfm);
-
-                deleteButton = new JButton("Delete");
-                editButton = new JButton("Edit");
-                titleButtonPanel.add(deleteButton);
-                titleButtonPanel.add(editButton);
-
-                deleteButton.addActionListener(e -> {
-                    backend.db.accountsMap.remove(accountList.getObjectUUID(account.getUsername()));
-                    parent.showPanel("accountsTable");
-                });
-
-                editButton.addActionListener(e -> {
-                    editcfm.setVisible(true);
-                    fieldUsername.setEditable(true);
-                    fieldPassword.setEditable(true);
-                    roleComboBox.setEnabled(true);
-                });
-
-                editConfirm.addActionListener(e -> {
-                    editcfm.setVisible(false);
-                    try {
-                        String username = fieldUsername.getData();
-                        String password = fieldPassword.getPassword();
-                        String roleString = (String) roleComboBox.getSelectedItem();
-
-                        if (!username.equals(account.getUsername())) {
-                            System.out.println("username changed"); // TODO: remove
-                            backend.db.accountsMap.get(accountList.getObjectUUID(account.getUsername())).setUsername(username);
-                        }
-                        if (!password.equals(account.getPassword())) {
-                            System.out.println("pass changed"); // TODO: remove
-                            backend.db.accountsMap.get(accountList.getObjectUUID(account.getUsername())).setPassword(password);
-                        }
-                        if (!role.equals(getRoleString(account.getRole()))) {
-                            System.out.println("role changed"); // TODO: remove
-                            backend.db.accountsMap.get(accountList.getObjectUUID(account.getUsername())).setRole(getRoleFromString(roleString));
-                        }
-
-                        System.out.println("item updated"); // TODO: remove
-                        fieldUsername.setEditable(false);
-                        fieldPassword.setEditable(false);
-                        roleComboBox.setEnabled(false);
-
-                        editcfm.setVisible(false);
-                    } catch (Exception err) {
-                        System.out.println(err);
-                    }
-
-                });
-
-                editCancel.addActionListener(e -> {
-                    fieldUsername.setEditable(false);
-                    fieldPassword.setEditable(false);
-                    roleComboBox.setEnabled(false);
-
-                    fieldUsername.setData(account.getUsername());
-                    fieldPassword.setPassword(account.getPassword());
-                    roleComboBox.setSelectedItem(getRoleString(account.getRole()));
-                    editcfm.setVisible(false);
-                });
-            }
-        }
-    }
-
-    public void setData() {
-        if (viewOnly) {
-            account = accountList.getObject(rowData);
-            System.out.println("yay"); // TODO: Remove
-            System.out.println(rowData); // TODO: Remove
-            System.out.println(accountList.getObject(rowData).getPassword()); // TODO: Remove
-
             fieldUsername.setData(account.getUsername());
             fieldPassword.setPassword(account.getPassword());
             roleComboBox.setSelectedItem(getRoleString(account.getRole()));
-        }
+            editcfm.setVisible(false);
+        });
+
+        deleteButton = new JButton("Delete");
+        editButton = new JButton("Edit");
+        titleButtonPanel.add(deleteButton);
+        titleButtonPanel.add(editButton);
+
+        deleteButton.addActionListener(e -> {
+            backend.db.accountsMap.remove(accountList.getObjectUUID(account.getUsername()));
+            parent.showPanel("accountsTable");
+        });
+
+        editButton.addActionListener(e -> {
+            editcfm.setVisible(true);
+            fieldPassword.setEditable(true);
+            roleComboBox.setEnabled(true);
+        });
+        
+        // init item lists
+        accountList.setItem(items);
+        accountList.setValue();
+        // If is a view only
+    
+
+        confirmButton = new JButton("Add User");
+        confirmButton.addActionListener(e -> {
+            // Logic to add the user
+            try {
+                String username = fieldUsername.getData();
+                String password = fieldPassword.getPassword();
+                String roleString = (String) roleComboBox.getSelectedItem();
+                // Ad thisser to the backend
+
+                if (!username.isEmpty() || !password.isEmpty()) {
+                    
+                    backend.db.addAccount(new Account(username, password, getRoleFromString(roleString)));
+                    greenLabel.setVisible(true);
+                    System.out.println("Adding user: " + username + ":" + password + ":" + getRoleFromString(roleString)); // TODO: Remove
+
+                    // Reset field and data
+                    fieldUsername.resetField();
+                    fieldPassword.resetField();
+                    role = null;
+
+                    // timer for showing green label
+                    Timer timer = new Timer(2000, g -> greenLabel.setVisible(false));
+                    timer.setRepeats(false);
+                    timer.start();
+                }
+            } catch (Exception err) {
+                System.out.println(err);
+            }
+
+        });
+        titleButtonPanel.add(confirmButton);
+
+    }
+
+    void createHideOrShow(boolean bool){
+        confirmButton.setVisible(bool);
+    }
+
+    void editorHideOrShow(boolean bool){
+        deleteButton.setVisible(bool);
+        editButton.setVisible(bool);
+    }
+    
+    void editHideOrShow(boolean bool){
+        editcfm.setVisible(bool);
+    }
+
+    public void CreateUser(){
+        createHideOrShow(true);
+        editorHideOrShow(false);
+        editHideOrShow(false);
+    }
+
+    public void viewOnly(){
+        createHideOrShow(false);
+        editorHideOrShow(false);
+        editHideOrShow(false);
+
+        fieldUsername.setEditable(false);
+        fieldPassword.setEditable(false);
+        roleComboBox.setEnabled(false);
+    }
+
+    public void viewUpdate(){
+        createHideOrShow(false);
+        editorHideOrShow(true);
+        editHideOrShow(false);
+    }
+
+    public void setData() {
+        System.out.println(rowData); // TODO: Remove
+        account = backend.db.getAccount(rowData);
+        System.out.println("yay"); // TODO: Remove
+        System.out.println(account.getPassword()); // TODO: Remove
+
+        fieldUsername.setData(account.getUsername());
+        fieldPassword.setPassword(account.getPassword());
+        roleComboBox.setSelectedItem(getRoleString(account.getRole()));
+        
     }
 
     public void setRowNum(String data) {
