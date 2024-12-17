f89cd672b82ea4c3b355b691dde060dd3a35c37e
meow-d <isaacwenyi@gmail.com>
2024-12-17 17:18:46 +0800

    Remove sales manager permission to see supplier

--- src/data/Role.java
+++ src/data/Role.java
@@ -33,7 +33,6 @@ public enum Role {
     private static Map<String, EnumSet<Permission>> buildSalesManagerPermissions() {
         Map<String, EnumSet<Permission>> permissions = new HashMap<>();
         permissions.put("Items", EnumSet.of(Permission.READ));
-        permissions.put("Suppliers", EnumSet.of(Permission.READ));
         permissions.put("Sales", EnumSet.of(Permission.READ));
         permissions.put("PurchaseRequisition", EnumSet.of(Permission.CREATE, Permission.READ));
         permissions.put("PurchaseOrder", EnumSet.of(Permission.READ));
